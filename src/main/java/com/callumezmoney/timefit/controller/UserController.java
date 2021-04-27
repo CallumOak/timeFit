4f97273706d89aa4c4b67ce144167d89137826a3
Callum <callumoakey@hotmail.com>
2021-04-27 19:10:52 +0200

    update controller to restcontrollers with cors

--- src/main/java/com/callumezmoney/timefit/controller/UserController.java
+++ src/main/java/com/callumezmoney/timefit/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.callumezmoney.timefit.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
 
