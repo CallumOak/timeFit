4281ee6088ec73e0ca562bcda7ffbee694715a75
Callum <callumoakey@hotmail.com>
2021-05-05 11:47:12 +0200

    wip: jwt security frontend class structure

--- angularTimeFit/src/app/app.module.ts
+++ angularTimeFit/src/app/app.module.ts
@@ -8,12 +8,26 @@ import { UserFormComponent } from './component/user-form/user-form.component';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import {UserService} from "./service/user.service";
+import { BoardAdminComponent } from './component/board-admin/board-admin.component';
+import { BoardUserComponent } from './component/board-user/board-user.component';
+import { BoardModeratorComponent } from './component/board-moderator/board-moderator.component';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { SignupComponent } from './component/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
-    UserFormComponent
+    UserFormComponent,
+    BoardAdminComponent,
+    BoardUserComponent,
+    BoardModeratorComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
