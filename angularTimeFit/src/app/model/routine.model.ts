58a3e6df9584424199aa6752db10d42ed1a715fc
Callum <oakeyca@cronos.be>
2022-05-19 12:10:23 +0200

    update frontend routine model

--- angularTimeFit/src/app/model/routine.model.ts
+++ angularTimeFit/src/app/model/routine.model.ts
@@ -2,10 +2,12 @@ import {Deserializable} from "../interfaces/deserializable";
 
 export class Routine /*implements Deserializable*/ {
   id!: string;
-  program!: string;
-  routine!: string;
-  startTime!: string;
-  endTime!: string;
+  user!: string;
+  name!: string;
+  numberOfCycles!: number;
+  color! : string;
+  exercises : string[] = [];
+  routinePlans : string[] = [];
 /*  deserialize(input: any): this {
     return undefined;
   }*/
